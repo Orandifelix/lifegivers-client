83ac9cfca4b9c799325a74719d4ba0358ce9dd4b
Orandifelix <125347734+Orandifelix@users.noreply.github.com>
2023-09-03 16:58:13 +0300

    styled footer

--- src/components/orandilanding/Footer.js
+++ src/components/orandilanding/Footer.js
@@ -1,4 +1,8 @@
 import React from "react";
+import { PiFacebookLogoBold } from "react-icons/pi";
+import { TiSocialTwitterCircular } from "react-icons/ti";
+import { BiLogoInstagram } from "react-icons/bi";
+import { RiYoutubeLine } from "react-icons/ri";
 
 const Footer = () => {
   return (
@@ -13,17 +17,32 @@ const Footer = () => {
       </ul>
       <div className="contacts">
         <div className="phone">You can call us on: 0200 123 23 23</div>
-        <div className="icons">Icons</div>
+        <div className="icons">
+          <a href="your-facebook-url" className="icon-link facebook">
+            <PiFacebookLogoBold size="2rem" />
+          </a>
+          <a href="your-twitter-url" className="icon-link">
+            <TiSocialTwitterCircular size="2rem" />
+          </a>
+          <a href="your-instagram-url" className="icon-link">
+            <BiLogoInstagram size="2rem" />
+          </a>
+          <a href="your-youtube-url" className="icon-link">
+            <RiYoutubeLine size="2rem" />
+          </a>
+        </div>
       </div>
       <div className="donation-links">
-        <button className="donation-button">Blood</button>
-        <button className="donation-button">Organs</button>
-        <button className="donation-button">Tissues</button>
-        <button className="donation-button">Platelets</button>
-        <button className="donation-button">Cord Blood</button>
-        <button className="donation-button">Bone Marrow</button>
+        <button className="donation-button blood">Blood</button>
+        <button className="donation-button organs">Organs</button>
+        <button className="donation-button tissues">Tissues</button>
+        <button className="donation-button platelets">Platelets</button>
+        <button className="donation-button cord-blood">Cord Blood</button>
+        <button className="donation-button bone-marrow">Bone Marrow</button>
       </div>
-      <div>Copyright</div>
+      <div className="copyright">
+        &copy; LifeGivers {new Date().getFullYear()}
+      </div>
     </footer>
   );
 };
