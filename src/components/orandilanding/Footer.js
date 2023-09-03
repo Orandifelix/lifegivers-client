22a17526048762e5136abf5ef8815189d5f48c00
Orandifelix <125347734+Orandifelix@users.noreply.github.com>
2023-09-03 18:54:42 +0300

    styled the  footer icons and  links

--- src/components/orandilanding/Footer.js
+++ src/components/orandilanding/Footer.js
@@ -8,27 +8,42 @@ const Footer = () => {
   return (
     <footer>
       <ul className="info">
-        <li>Contact Us</li>
-        <li>Accessibility</li>
-        <li>Cookies</li>
-        <li>Privacy</li>
-        <li>Jobs</li>
-        <li>Diversity and Inclusion</li>
+        <li>
+          <a href="/contact-us">Contact Us</a>
+        </li>
+        <li>
+          <a href="/accessibility">Accessibility</a>
+        </li>
+        <li>
+          <a href="/cookies">Cookies</a>
+        </li>
+        <li>
+          <a href="/privacy">Privacy</a>
+        </li>
+        <li>
+          <a href="/jobs">Jobs</a>
+        </li>
+        <li>
+          <a href="/diversity-and-inclusion">Diversity and Inclusion</a>
+        </li>
       </ul>
       <div className="contacts">
-        <div className="phone">You can call us on: 0200 123 23 23</div>
+        <div className="phone">
+          You can call us on:
+          <br /> <span>0200 123 23 23</span>
+        </div>
         <div className="icons">
           <a href="your-facebook-url" className="icon-link facebook">
-            <PiFacebookLogoBold size="2rem" />
+            <PiFacebookLogoBold size="2.5rem" color="rgb(58,90,153)" />
           </a>
           <a href="your-twitter-url" className="icon-link">
-            <TiSocialTwitterCircular size="2rem" />
+            <TiSocialTwitterCircular size="2.5rem" color="rgb(37,169,224)" />
           </a>
           <a href="your-instagram-url" className="icon-link">
-            <BiLogoInstagram size="2rem" />
+            <BiLogoInstagram size="2.5rem" color="rgb( 131, 58, 180)" />
           </a>
           <a href="your-youtube-url" className="icon-link">
-            <RiYoutubeLine size="2rem" />
+            <RiYoutubeLine size="2.5rem" color="red" />
           </a>
         </div>
       </div>
