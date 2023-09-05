3f6ea3f9d6effef868e13f049ba73770b479cd46
Orandifelix <125347734+Orandifelix@users.noreply.github.com>
2023-09-05 10:48:53 +0300

    Moved Routes to App.js

--- src/components/orandilanding/landitems/Aboutus.js
+++ src/components/orandilanding/landitems/Aboutus.js
@@ -1,16 +1,8 @@
 import React from 'react'
-import Landingcard from './Landingcard'
-import Landingimg from './Landingimg'
 const Aboutus = () => {
   return (
     <div>
-      <div>
-        <Landingimg />
-      </div>
-
-      <Landingcard />
-      <Landingcard />
-      <Landingcard />
+Heloo
     </div>
   );
 }
