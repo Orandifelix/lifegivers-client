a89f178e615b196fc076e04fe2a27433b3dc8a04
Kevin-kinyanjui <kevin.kinyanjui@student.moringaschool.com>
2023-09-06 13:24:58 +0300

    cleared signup error

--- src/components/kevinauth/SignUpLogin.js
+++ src/components/kevinauth/SignUpLogin.js
@@ -15,7 +15,7 @@ class SignUpLoginComponent extends Component {
       email: "",
       contact_number: "",
       isSignUp: true,
-      error: null,
+      error: null, // Define the error variable in your state
     };
   }
 
@@ -28,6 +28,7 @@ class SignUpLoginComponent extends Component {
     event.preventDefault();
     try {
       if (this.state.isSignUp) {
+        // Handle signup
         const userData = {
           user: {
             username: this.state.username,
@@ -40,7 +41,7 @@ class SignUpLoginComponent extends Component {
             contact_number: this.state.contact_number,
           },
         };
-
+        
         const response = await fetch('http://localhost:3000/users', {
           method: 'POST',
           headers: {
@@ -50,28 +51,19 @@ class SignUpLoginComponent extends Component {
         });
 
         if (!response.ok) {
-          const errorData = await response.json();
-          throw new Error(errorData.message || 'Failed to sign up');
+          // Handle the case where the response status is not in the range 200-299
+          throw new Error(`Request failed with status ${response.status}`);
         }
 
-        // Reset form and clear errors on success
-        this.setState({
-          name: "",
-          gender: "",
-          age: "",
-          weight: "",
-          email: "",
-          contact_number: "",
-          error: null,
-        });
-
-        console.log('Sign up successful');
+        const data = await response.json();
+        console.log(data);
       } else {
+        // Handle login
         const loginData = {
           username: this.state.username,
           password: this.state.password,
         };
-
+        
         const response = await fetch('http://localhost:3000/login', {
           method: 'POST',
           headers: {
@@ -81,17 +73,12 @@ class SignUpLoginComponent extends Component {
         });
 
         if (!response.ok) {
-          const errorData = await response.json();
-          throw new Error(errorData.message || 'Failed to log in');
+          // Handle the case where the response status is not in the range 200-299
+          throw new Error(`Request failed with status ${response.status}`);
         }
 
-        // Reset form and clear errors on success
-        this.setState({
-          password: "",
-          error: null,
-        });
-
-        console.log('Log in successful');
+        const data = await response.json();
+        console.log(data);
       }
     } catch (error) {
       // Handle and store errors
@@ -105,128 +92,127 @@ class SignUpLoginComponent extends Component {
   };
 
   render() {
-    const { isSignUp } = this.state;
+    const { isSignUp, error } = this.state;
 
     return (
       <>
-      {/* <Navbar /> */}
-      <Container>
-        <br />
-        <Row className="justify-content-center">
-          <Col md={6}>
-            <Form onSubmit={this.handleSubmit}>
-            {error && <div className="alert alert-danger">{error}</div>}
-              {isSignUp && (
-                <>
-                  <Form.Group controlId="name">
-                    <Form.Label>Name</Form.Label>
-                    <Form.Control
-                      type="text"
-                      name="name"
-                      value={this.state.name}
-                      onChange={this.handleChange}
-                      required
-                    />
-                  </Form.Group>
-                  <Form.Group controlId="gender">
-                  <Form.Label>Gender</Form.Label>
+        <Container>
+          <br />
+          <Row className="justify-content-center">
+            <Col md={6}>
+              <Form onSubmit={this.handleSubmit}>
+                {isSignUp && (
+                  <>
+                    <Form.Group controlId="name">
+                      <Form.Label>Name</Form.Label>
+                      <Form.Control
+                        type="text"
+                        name="name"
+                        value={this.state.name}
+                        onChange={this.handleChange}
+                        required
+                      />
+                    </Form.Group>
+                    <Form.Group controlId="gender">
+                      <Form.Label>Gender</Form.Label>
+                      <Form.Control
+                        as="select"
+                        name="gender"
+                        value={this.state.gender}
+                        onChange={this.handleChange}
+                        required
+                      >
+                        <option value="">Select Gender</option>
+                        <option value="Male">Male</option>
+                        <option value="Female">Female</option>
+                      </Form.Control>
+                    </Form.Group>
+                    <Form.Group controlId="age">
+                      <Form.Label>Age</Form.Label>
+                      <Form.Control
+                        type="number"
+                        name="age"
+                        value={this.state.age}
+                        onChange={this.handleChange}
+                        required
+                      />
+                    </Form.Group>
+                    <Form.Group controlId="weight">
+                      <Form.Label>Weight</Form.Label>
+                      <Form.Control
+                        type="number"
+                        name="weight"
+                        value={this.state.weight}
+                        onChange={this.handleChange}
+                        required
+                      />
+                    </Form.Group>
+                    <Form.Group controlId="email">
+                      <Form.Label>Email</Form.Label>
+                      <Form.Control
+                        type="email"
+                        name="email"
+                        value={this.state.email}
+                        onChange={this.handleChange}
+                        required
+                      />
+                    </Form.Group>
+                    <Form.Group controlId="contact_number">
+                      <Form.Label>Contact Number</Form.Label>
+                      <Form.Control
+                        type="tel"
+                        name="contact_number"
+                        value={this.state.contact_number}
+                        onChange={this.handleChange}
+                        required
+                      />
+                    </Form.Group>
+                  </>
+                )}
+                <Form.Group controlId="username">
+                  <Form.Label>Username</Form.Label>
                   <Form.Control
-                    as="select"
-                    name="gender"
-                    value={this.state.gender}
+                    type="text"
+                    name="username"
+                    value={this.state.username}
                     onChange={this.handleChange}
                     required
-                  >
-                    <option value="">Select Gender</option>
-                    <option value="Male">Male</option>
-                    <option value="Female">Female</option>
-                  </Form.Control>
+                  />
                 </Form.Group>
-                  <Form.Group controlId="age">
-                    <Form.Label>Age</Form.Label>
-                    <Form.Control
-                      type="number"
-                      name="age"
-                      value={this.state.age}
-                      onChange={this.handleChange}
-                      required
-                    />
-                  </Form.Group>
-                  <Form.Group controlId="weight">
-                    <Form.Label>Weight</Form.Label>
-                    <Form.Control
-                      type="number"
-                      name="weight"
-                      value={this.state.weight}
-                      onChange={this.handleChange}
-                      required
-                    />
-                  </Form.Group>
-                  <Form.Group controlId="email">
-                    <Form.Label>Email</Form.Label>
-                    <Form.Control
-                      type="email"
-                      name="email"
-                      value={this.state.email}
-                      onChange={this.handleChange}
-                      required
-                    />
-                  </Form.Group>
-                  <Form.Group controlId="contact_number">
-                    <Form.Label>Contact Number</Form.Label>
-                    <Form.Control
-                      type="tel"
-                      name="contact_number"
-                      value={this.state.contact_number}
-                      onChange={this.handleChange}
-                      required
-                    />
-                  </Form.Group>
-                </>
-              )}
-              <Form.Group controlId="username">
-                <Form.Label>Username</Form.Label>
-                <Form.Control
-                  type="text"
-                  name="username"
-                  value={this.state.username}
-                  onChange={this.handleChange}
-                  required
-                />
-              </Form.Group>
-              <Form.Group controlId="password">
-                <Form.Label>Password</Form.Label>
-                <Form.Control
-                  type="password"
-                  name="password"
-                  value={this.state.password}
-                  onChange={this.handleChange}
-                  required
-                />
-              </Form.Group>
-              <br />
+                <Form.Group controlId="password">
+                  <Form.Label>Password</Form.Label>
+                  <Form.Control
+                    type="password"
+                    name="password"
+                    value={this.state.password}
+                    onChange={this.handleChange}
+                    required
+                  />
+                </Form.Group>
+                <br />
                 <Row>
                   <Col className="text-left">
                     <Button variant="primary" type="submit">
-                    {isSignUp ? "Sign Up" : "Login"}
+                      {isSignUp ? "Sign Up" : "Login"}
                     </Button>
                   </Col>
                   <Col className="text-right">
                     <Button
-                    variant="secondary"
-                    onClick={this.toggleForm}
-                    className="ml-2"
+                      variant="secondary"
+                      onClick={this.toggleForm}
+                      className="ml-2"
                     >
-                    {isSignUp ? "Switch to Login" : "Switch to Sign Up"}
+                      {isSignUp ? "Switch to Login" : "Switch to Sign Up"}
                     </Button>
                   </Col>
                 </Row>
                 <br />
-            </Form>
-          </Col>
-        </Row>
-      </Container>
-      <br />
+              </Form>
+              {error && <div className="alert alert-danger">{error}</div>}
+            </Col>
+          </Row>
+        </Container>
+        <br />
       </>
     );
   }
