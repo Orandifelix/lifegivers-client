bd46b96c8e5f013dffb7320dd2a26a53f3dd1891
leilaotieno2 <leila.otieno@student.moringaschool.com>
2023-09-06 22:43:14 +0300

    donations form

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import "./App.css";
+import React from "react";
 import SignUpLoginComponent from "./components/kevinauth/SignUpLogin";
-import Articles from "./components/leila /articles";
+import Articles from "./components/leiladonations/articles"; // Updated import path
 import Navbar from "./components/orandilanding/Navbar";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Aboutus from "./components/orandilanding/landitems/Aboutus";
 import Footer from "./components/orandilanding/Footer";
+import DonationForm from "./components/leiladonations/DonationForm"; // Updated import without .js extension
 
 import DonationCenterList from "./components/kevinauth/DonationCenterList";
 import DonationCenterUpdate from "./components/kevinauth/DonationCenterUpdate";
@@ -20,19 +21,12 @@ function App() {
       <Routes>
         <Route path="/login" element={<SignUpLoginComponent />} />
         <Route path="/donation_centers" element={<DonationCenterList />} />
-        <Route  path="/" element={<Landing/>}/>
-        <Route
-          path="/donation_centers/update"
-          element={<DonationCenterUpdate />}
-        />
-        <Route
-          path="/donation_centers/create"
-          element={<DonationCenterCreate />}
-        />
+        <Route path="/DonationForm" element={<DonationForm />} /> {/* Updated route */}
+        <Route path="/" element={<Landing />} />
+        <Route path="/donation_centers/update" element={<DonationCenterUpdate />} />
+        <Route path="/donation_centers/create" element={<DonationCenterCreate />} />
         <Route path="/aboutus" element={<Aboutus />} />
-
-        <Route path="/articles" element={<Articles />} />
-
+        <Route path="/articles" element={<Articles />} /> {/* Updated route */}
         <Route path="/types" element={<Donationtypes />} />
       </Routes>
       <hr className="horizontal-line" />
---
+++ src/components/leiladonations/DonationForm.js
@@ -0,0 +1,145 @@
+import React, { Component } from "react";
+import { Form, Button, Container, Row, Col } from "react-bootstrap";
+
+class DonationForm extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      donationType: "",
+      donationDate: "",
+      donationTime: "",
+      donationStatus: "",
+      transactionNumber: "",
+      errors: [],
+    };
+  }
+
+  handleChange = (event) => {
+    const { name, value } = event.target;
+    this.setState({ [name]: value });
+  };
+
+  handleSubmit = async (event) => {
+    event.preventDefault();
+
+    // Handle donation submission
+    const donationData = {
+      donationType: this.state.donationType,
+      donationDate: this.state.donationDate,
+      donationTime: this.state.donationTime,
+      donationStatus: this.state.donationStatus,
+      transactionNumber: this.state.transactionNumber,
+    };
+
+    // Add your fetch request to submit the donation data to your backend here
+    // You should replace the URL and add appropriate headers and error handling
+    try {
+      const response = await fetch("YOUR_API_URL", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(donationData),
+      });
+
+      if (response.ok) {
+        // Donation successfully submitted
+        console.log("Donation submitted successfully");
+      } else {
+        // Handle errors here
+        const errorData = await response.json();
+        this.setState({ errors: errorData.errors });
+        console.error("Donation submission failed");
+      }
+    } catch (error) {
+      console.error("Error:", error);
+    }
+  };
+
+  render() {
+    const { errors } = this.state;
+
+    return (
+      <Container>
+        <br />
+        <Row className="justify-content-center">
+          <Col md={6}>
+            {errors.length > 0 && (
+              <div className="alert alert-danger">
+                <ul>
+                  {errors.map((error, index) => (
+                    <li key={index}>{error}</li>
+                  ))}
+                </ul>
+              </div>
+            )}
+            <Form onSubmit={this.handleSubmit}>
+              <Form.Group controlId="donationType">
+                <Form.Label>Donation Type</Form.Label>
+                <Form.Control
+                  type="text"
+                  name="donationType"
+                  value={this.state.donationType}
+                  onChange={this.handleChange}
+                  required
+                />
+              </Form.Group>
+              <Form.Group controlId="donationDate">
+                <Form.Label>Donation Date</Form.Label>
+                <Form.Control
+                  type="date"
+                  name="donationDate"
+                  value={this.state.donationDate}
+                  onChange={this.handleChange}
+                  required
+                />
+              </Form.Group>
+              <Form.Group controlId="donationTime">
+                <Form.Label>Donation Time</Form.Label>
+                <Form.Control
+                  type="time"
+                  name="donationTime"
+                  value={this.state.donationTime}
+                  onChange={this.handleChange}
+                  required
+                />
+              </Form.Group>
+              <Form.Group controlId="donationStatus">
+                <Form.Label>Donation Status</Form.Label>
+                <Form.Control
+                  type="text"
+                  name="donationStatus"
+                  value={this.state.donationStatus}
+                  onChange={this.handleChange}
+                  required
+                />
+              </Form.Group>
+              <Form.Group controlId="transactionNumber">
+                <Form.Label>Transaction Number</Form.Label>
+                <Form.Control
+                  type="text"
+                  name="transactionNumber"
+                  value={this.state.transactionNumber}
+                  onChange={this.handleChange}
+                  required
+                />
+              </Form.Group>
+              <br />
+              <Row>
+                <Col className="text-left">
+                  <Button variant="primary" type="submit">
+                    Submit Donation
+                  </Button>
+                </Col>
+              </Row>
+              <br />
+            </Form>
+          </Col>
+        </Row>
+      </Container>
+    );
+  }
+}
+
+export default DonationForm;
