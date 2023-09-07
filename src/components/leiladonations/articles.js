cfd7a6c914793090f00386c4843aa0b266e35aa1
leilaotieno2 <leila.otieno@student.moringaschool.com>
2023-09-07 14:22:13 +0300

    articles

--- src/components/leiladonations/articles.js
+++ src/components/leiladonations/articles.js
@@ -1,152 +1,22 @@
-import React, { Component } from "react";
-import { Form, Button, Container, Row, Col } from "react-bootstrap";
+import React from "react";
+import "./components/leiladonations/articles.css"; // Update the CSS file path accordingly
+import articlesData from "./components/leiladonations/articles1.js"; // Import the data from Article1.js
 
-class Article extends Component {
-  constructor(props) {
-    super(props);
+const Articles = () => {
+  return (
+    <>
+      <div className="articles">
+        {/* Map through the articlesData and create cards for each article */}
+        {articlesData.map((article) => (
+          <div className="article-card" key={article.id}>
+            <h3>{article.title}</h3>
+            <p>{article.content}</p>
+            {/* You can add more elements here, like images, links, etc. */}
+          </div>
+        ))}
+      </div>
+    </>
+  );
+};
 
-    this.state = {
-      title: "",
-      content: "",
-      donor_name: "",
-      donation_type_id: "", // Assuming this is a dropdown/select input
-      donation_photo: "",
-      author: "",
-      errors: [],
-    };
-  }
-
-  handleChange = (event) => {
-    const { name, value } = event.target;
-    this.setState({ [name]: value });
-  };
-
-  handleSubmit = async (event) => {
-    event.preventDefault();
-
-    // Add your fetch request to submit the article data to your backend here
-    // You should replace the URL and add appropriate headers and error handling
-    try {
-      const response = await fetch("YOUR_API_URL", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(this.state),
-      });
-
-      if (response.ok) {
-        // Article successfully submitted
-        console.log("Article submitted successfully");
-      } else {
-        // Handle errors here
-        const errorData = await response.json();
-        this.setState({ errors: errorData.errors });
-        console.error("Article submission failed");
-      }
-    } catch (error) {
-      console.error("Error:", error);
-    }
-  };
-
-  render() {
-    const { errors } = this.state;
-
-    return (
-      <Container>
-        <br />
-        <Row className="justify-content-center">
-          <Col md={6}>
-            {errors.length > 0 && (
-              <div className="alert alert-danger">
-                <ul>
-                  {errors.map((error, index) => (
-                    <li key={index}>{error}</li>
-                  ))}
-                </ul>
-              </div>
-            )}
-            <Form onSubmit={this.handleSubmit}>
-              <Form.Group controlId="title">
-                <Form.Label>Title</Form.Label>
-                <Form.Control
-                  type="text"
-                  name="title"
-                  value={this.state.title}
-                  onChange={this.handleChange}
-                  required
-                />
-              </Form.Group>
-              <Form.Group controlId="content">
-                <Form.Label>Content</Form.Label>
-                <Form.Control
-                  as="textarea"
-                  name="content"
-                  value={this.state.content}
-                  onChange={this.handleChange}
-                  required
-                />
-              </Form.Group>
-              <Form.Group controlId="donor_name">
-                <Form.Label>Donor Name</Form.Label>
-                <Form.Control
-                  type="text"
-                  name="donor_name"
-                  value={this.state.donor_name}
-                  onChange={this.handleChange}
-                  required
-                />
-              </Form.Group>
-              <Form.Group controlId="donation_type_id">
-                <Form.Label>Donation Type</Form.Label>
-                <Form.Control
-                  as="select"
-                  name="donation_type_id"
-                  value={this.state.donation_type_id}
-                  onChange={this.handleChange}
-                  required
-                >
-                  {/* Add options for donation types */}
-                  <option value="1">Type 1</option>
-                  <option value="2">Type 2</option>
-                  {/* Add more options as needed */}
-                </Form.Control>
-              </Form.Group>
-              <Form.Group controlId="donation_photo">
-                <Form.Label>Donation Photo (URL)</Form.Label>
-                <Form.Control
-                  type="text"
-                  name="donation_photo"
-                  value={this.state.donation_photo}
-                  onChange={this.handleChange}
-                  required
-                />
-              </Form.Group>
-              <Form.Group controlId="author">
-                <Form.Label>Author</Form.Label>
-                <Form.Control
-                  type="text"
-                  name="author"
-                  value={this.state.author}
-                  onChange={this.handleChange}
-                  required
-                />
-              </Form.Group>
-              <br />
-              <Row>
-                <Col className="text-left">
-                  <Button variant="primary" type="submit">
-                    Submit Article
-                  </Button>
-                </Col>
-              </Row>
-              <br />
-            </Form>
-          </Col>
-        </Row>
-      </Container>
-    );
-  }
-}
-
-export default Article;
+export default Articles;
--- src/components/leiladonations/articles1.js
+++ src/components/leiladonations/articles1.js
@@ -1,28 +1,29 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import { Form, Button, Container, Row, Col } from "react-bootstrap";
+import { Link } from 'react-router-dom';
+function Article1() {
+  const [formData, setFormData] = useState({
+    title: "",
+    content: "",
+    donor_name: "",
+    donation_type_id: "",
+    donation_photo: "",
+    author: "",
+  });
 
-class Article1 extends Component {
-  constructor(props) {
-    super(props);
+  const [errors, setErrors] = useState([]); // Ensure it's initialized as an empty array
 
-    this.state = {
-      title: "",
-      content: "",
-      donor_name: "",
-      donation_type_id: "", // Assuming this is a dropdown/select input
-      donation_photo: "",
-      author: "",
-      errors: [],
-      successMessage: "",
-    };
-  }
+  const [successMessage, setSuccessMessage] = useState("");
 
-  handleChange = (event) => {
+  const handleChange = (event) => {
     const { name, value } = event.target;
-    this.setState({ [name]: value });
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
   };
 
-  handleSubmit = async (event) => {
+  const handleSubmit = async (event) => {
     event.preventDefault();
 
     try {
@@ -32,133 +33,121 @@ class Article1 extends Component {
           "Content-Type": "application/json",
         },
         body: JSON.stringify({
-          article: { // Match the expected structure in your Rails controller
-            title: this.state.title,
-            content: this.state.content,
-            donor_name: this.state.donor_name,
-            donation_type_id: this.state.donation_type_id,
-            donation_photo: this.state.donation_photo,
-            author: this.state.author,
+          article: {
+            ...formData,
           },
         }),
       });
 
       if (response.ok) {
-        // Article successfully submitted
-        this.setState({ successMessage: "Article submitted successfully" });
+        setSuccessMessage("Article submitted successfully");
+        setErrors([]); // Reset errors to an empty array on success
       } else {
-        // Handle errors here
         const errorData = await response.json();
-        this.setState({ errors: errorData.errors });
+        setErrors(errorData.errors || []); // Ensure errors is always an array
       }
     } catch (error) {
       console.error("Error:", error);
     }
   };
 
-  render() {
-    const { errors, successMessage } = this.state;
-
-    return (
-      <Container>
-        <br />
-        <Row className="justify-content-center">
-          <Col md={6}>
-            {successMessage && (
-              <div className="alert alert-success">
-                {successMessage}
-              </div>
-            )}
-            {errors.length > 0 && (
-              <div className="alert alert-danger">
-                <ul>
-                  {errors.map((error, index) => (
-                    <li key={index}>{error}</li>
-                  ))}
-                </ul>
-              </div>
-            )}
-               <Form onSubmit={this.handleSubmit}>
-              <Form.Group controlId="title">
-                <Form.Label>Title</Form.Label>
-                <Form.Control
-                  type="text"
-                  name="title"
-                  value={this.state.title}
-                  onChange={this.handleChange}
-                  required
-                />
-              </Form.Group>
-              <Form.Group controlId="content">
-                <Form.Label>Content</Form.Label>
-                <Form.Control
-                  as="textarea"
-                  name="content"
-                  value={this.state.content}
-                  onChange={this.handleChange}
-                  required
-                />
-              </Form.Group>
-              <Form.Group controlId="donor_name">
-                <Form.Label>Donor Name</Form.Label>
-                <Form.Control
-                  type="text"
-                  name="donor_name"
-                  value={this.state.donor_name}
-                  onChange={this.handleChange}
-                  required
-                />
-              </Form.Group>
-              <Form.Group controlId="donation_type_id">
-                <Form.Label>Donation Type</Form.Label>
-                <Form.Control
-                  as="select"
-                  name="donation_type_id"
-                  value={this.state.donation_type_id}
-                  onChange={this.handleChange}
-                  required
-                >
-                  {/* Add options for donation types */}
-                  <option value="1">Type 1</option>
-                  <option value="2">Type 2</option>
-                  {/* Add more options as needed */}
-                </Form.Control>
-              </Form.Group>
-              <Form.Group controlId="donation_photo">
-                <Form.Label>Donation Photo (URL)</Form.Label>
-                <Form.Control
-                  type="text"
-                  name="donation_photo"
-                  value={this.state.donation_photo}
-                  onChange={this.handleChange}
-                  required
-                />
-              </Form.Group>
-              <Form.Group controlId="author">
-                <Form.Label>Author</Form.Label>
-                <Form.Control
-                  type="text"
-                  name="author"
-                  value={this.state.author}
-                  onChange={this.handleChange}
-                  required
-                />
-              </Form.Group>
-              <br />
-              <Row>
-                <Col className="text-left">
-                  <Button variant="primary" type="submit">
-                    Submit Article
-                  </Button>
-                </Col>
-              </Row>
-              <br />
-            </Form>
-          </Col>
-        </Row>
-      </Container>
-    );
-  }
+  return (
+    <Container>
+      <br />
+      <Row className="justify-content-center">
+        <Col md={6}>
+          {successMessage && (
+            <div className="alert alert-success">{successMessage}</div>
+          )}
+          {errors.length > 0 && (
+            <div className="alert alert-danger">
+              <ul>
+                {errors.map((error, index) => (
+                  <li key={index}>{error}</li>
+                ))}
+              </ul>
+            </div>
+          )}
+       <Form onSubmit={handleSubmit}>
+            <Form.Group controlId="title">
+              <Form.Label>Title</Form.Label>
+              <Form.Control
+                type="text"
+                name="title"
+                value={formData.title}
+                onChange={handleChange}
+                required
+              />
+            </Form.Group>
+            <Form.Group controlId="content">
+              <Form.Label>Content</Form.Label>
+              <Form.Control
+                as="textarea"
+                name="content"
+                value={formData.content}
+                onChange={handleChange}
+                required
+              />
+            </Form.Group>
+            <Form.Group controlId="donor_name">
+              <Form.Label>Donor Name</Form.Label>
+              <Form.Control
+                type="text"
+                name="donor_name"
+                value={formData.donor_name}
+                onChange={handleChange}
+                required
+              />
+            </Form.Group>
+            <Form.Group controlId="donation_type_id">
+              <Form.Label>Donation Type</Form.Label>
+              <Form.Control
+                as="select"
+                name="donation_type_id"
+                value={formData.donation_type_id}
+                onChange={handleChange}
+                required
+              >
+                <option value="1">Type 1</option>
+                <option value="2">Type 2</option>
+              </Form.Control>
+            </Form.Group>
+            <Form.Group controlId="donation_photo">
+              <Form.Label>Donation Photo (URL)</Form.Label>
+              <Form.Control
+                type="text"
+                name="donation_photo"
+                value={formData.donation_photo}
+                onChange={handleChange}
+                required
+              />
+            </Form.Group>
+            <Form.Group controlId="author">
+              <Form.Label>Author</Form.Label>
+              <Form.Control
+                type="text"
+                name="author"
+                value={formData.author}
+                onChange={handleChange}
+                required
+              />
+            </Form.Group>
+            <br />
+            <Row>
+              <Col className="text-left">
+              <Link to="/articles">
+  <Button variant="primary" type="submit">
+    Submit Article
+  </Button>
+</Link>
+              </Col>
+            </Row>
+            <br />
+          </Form>
+        </Col>
+      </Row>
+    </Container>
+  );
 }
 
 export default Article1;
